3d3cbc0793ebeb1f5eddbbfcb34208ff2097df4a
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 17:19:30 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -30,14 +30,14 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to
+  shipping_manifest["muskets"] = 2
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+shipping_manifest["gunpowder"] = 4
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
